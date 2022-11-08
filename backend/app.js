fa4b48b63c93a7e3e318afaa6094857f76086831
Shih-Wei.Chung <Shih-Wei.Chung@xsquareiot.com>
2022-11-08 10:41:31 +0800

    feat: initial get and post route

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res){
 
-    res.send('Hello World');
+    res.sendFile(__dirname+'/index.html');
+});
+
+app.post('/',function(req, res){
+
+    console.log(req.body);
+    res.send('Thank for passing that');
 });
 
 app.listen(3000, function(){
